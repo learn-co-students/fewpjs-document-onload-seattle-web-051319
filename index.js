2f9c135daae85b488a59742d2d9996873f7646ad
chetchopra <chetanya.chopra94@gmail.com>
2019-06-24 14:30:29 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Your code goes here
+document.addEventListener("DOMContentLoaded", () => {
+  changeText()
+});
+
+
+
+function changeText() {
+  let text = document.querySelector("#text")
+  text.innerHTML = "This is really cool!"
+}
+
+
