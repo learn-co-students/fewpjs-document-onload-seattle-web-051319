90fd5456591a285e84ec41ccdcd57e3043520a7e
fut0k0 <cccabell@icloud.com>
2019-06-26 08:09:48 -0700

    Passing lab

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 document.addEventListener("DOMContentLoaded", function() {
-	document.getElementById("text").textContent = "This is really cool!"
+	document.getElementById("text").textContent = "This is really cool!";
 });
